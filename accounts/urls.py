061a984a98257acef2aa6471884c67fdc49ae55e
Pablo Palácios <_@pablopalacios.me>
2016-04-20 11:53:17 -0300

    accounts: implement logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url('^login/$', views.LoginView.as_view(), name='login'),
+    url('^logout/$', views.LogoutView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
-from authtools.views import LoginView as ATLoginView
+from authtools import views as at_views
+from django.core.urlresolvers import reverse_lazy
 
 
-class LoginView(ATLoginView):
+class LoginView(at_views.LoginView):
     template_name = 'accounts/login.html'
+
+
+class LogoutView(at_views.LogoutView):
+    url = reverse_lazy('accounts:login')
