989fb727b749adb01dfca8fbd1d7e5319cdb8b9f
Pablo Palácios <_@pablopalacios.me>
2016-04-20 18:28:30 -0300

    cleaned project

--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-
-urlpatterns = [
-    url('^users/$', views.UsersView.as_view(), name='users'),
-    url('^admin/$', views.AdminView.as_view(), name='admin'),
-]
