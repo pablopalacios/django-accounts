1cb37f4589cb729d20f17e3ce44a8706e0d79158
Pablo Palácios <_@pablopalacios.me>
2016-04-22 15:30:58 -0300

    models: add tests for gravatar feature

--- models.py
+++ models.py
@@ -7,8 +7,11 @@ from authtools.models import AbstractNamedUser
 
 class User(AbstractNamedUser):
 
-    def gravatar(self, size=80, default='mm'):
+    def get_gravatar_hash(self):
         email = self.email.lower().encode()
         hash = md5(email).hexdigest()
+        return hash
+
+    def gravatar(self, size=80, default='mm'):
         url = 'http://www.gravatar.com/avatar/{hash}?size={size}&d={default}'
-        return url.format(size=size, hash=hash, default=default)
+        return url.format(size=size, hash=self.get_gravatar_hash(), default=default)
--- tests.py
+++ tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email='ppalacios992@gmail.com', name='pablo')
+
+    def test_gravatar_hash(self):
+        expected = '7a406dae1a1c2c09818bdfe5c60bd4ae'
+        observed = self.user.get_gravatar_hash()
+        self.assertEqual(expected, observed)
+
+    def test_gravatar_url(self):
+        expected = 'http://www.gravatar.com/avatar/7a406dae1a1c2c09818bdfe5c60bd4ae?size=80&d=mm'
+        observed = self.user.gravatar()
+        self.assertEqual(expected, observed)
