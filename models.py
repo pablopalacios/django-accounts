196eaacb0703998627a91ce76b11d59d60217458
Pablo Palácios <_@pablopalacios.me>
2016-04-22 14:51:31 -0300

    models: add gravatar feature

--- models.py
+++ models.py
@@ -1,7 +1,14 @@
+from hashlib import md5
+
 from django.db import models
 
 from authtools.models import AbstractNamedUser
 
 
 class User(AbstractNamedUser):
-    pass
+
+    def gravatar(self, size=80, default='mm'):
+        email = self.email.lower().encode()
+        hash = md5(email).hexdigest()
+        url = 'http://www.gravatar.com/avatar/{hash}?size={size}&d={default}'
+        return url.format(size=size, hash=hash, default=default)
