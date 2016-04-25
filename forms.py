6c52c980583cac9ae62ae82c3955b25cdcd4a099
Pablo Palácios <_@pablopalacios.me>
2016-04-25 12:02:16 -0300

    forms: customized login form widgets

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib import auth
 from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext_lazy as _
 
 
 User = auth.get_user_model()
@@ -8,7 +9,26 @@ User = auth.get_user_model()
 
 class AuthForm(AuthenticationForm):
     """ Authentication form with remember me functionality """
-    remember = forms.BooleanField(required=False)
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': _('Email'),
+        }),
+        max_length=25)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': _('Password'),
+        })
+    )
+    remember_me = forms.BooleanField(
+        required=False,
+        label=_('Remember me'),
+        attrs={
+            'class': 'icheck',
+        },
+    )
 
 
 class ProfileUpdate(forms.ModelForm):
